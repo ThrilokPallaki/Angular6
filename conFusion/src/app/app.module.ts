4795855c4b07757fa10128ba8c03f914afb5e3d5
thrilokkumarpallaki <thrilokkumarpallaki@gmail.com>
2018-06-30 19:56:18 +0530

    Added header and footer components

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -18,12 +18,16 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 // importing services
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
